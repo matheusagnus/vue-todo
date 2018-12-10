6cbebc76fe7330012bec47816222c014c04e9d8c
matheusagnus <ferreira.zx@gmail.com>
2018-12-10 13:01:01 -0200

    put this on air

---
+++ src/components/Popup.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-dialog max-width="600px">
+    <v-btn flat slot="activator" class="success">Add New Project</v-btn>
+    <v-card>
+      <v-card-title>
+        <h2>Add a New Project</h2>
+      </v-card-title>
+      <v-card-text>
+        <v-form class="px-3">
+          <v-text-field v-model="title" label="Title" prepend-icon="folder"></v-text-field>
+          <v-textarea v-model="content" label="Information" prepend-icon="edit"></v-textarea>
+
+            <v-menu>
+                <v-text-field :value="formattedDate" slot="activator" label="Due date" prepend-icon="date_range"></v-text-field>
+                <v-date-picker v-model="due"></v-date-picker>
+            </v-menu>
+
+          <v-spacer></v-spacer>
+          
+          <v-btn flat @click="submit" class="success mx-0 mt-3">Add Project</v-btn>
+        </v-form>
+      </v-card-text>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+import format from 'date-fns/format'
+
+export default {
+  data() {
+    return {
+      title: '',
+      content: '',
+      due: null
+    }
+  },
+  methods: {
+    submit() {
+      console.log(this.title, this.content)
+    }
+  },
+  computed: {
+      formattedDate() {
+          return this.due ? format(this.due, 'Do MMM YYYY') : ''
+      }
+  }
+}
+</script>
